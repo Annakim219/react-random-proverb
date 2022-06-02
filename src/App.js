ab617b291113fa8ea6e06e3d39a34303ca75f1d4
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-06-02 12:37:02 +0900

    create react app 실습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const proverbs =['좌절감으로 배움을 늦추지 마라.', '기타등등', '기타등등드르릉']
+
+  const getRandomIndex = (length)=>{
+    return parseInt(Math.random()*length);
+  }  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> 파일을 고치고 저장하면 새로운 명언을 볼 수 있습니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+         {proverbs[getRandomIndex(proverbs.length)]}
+
       </header>
     </div>
   );
